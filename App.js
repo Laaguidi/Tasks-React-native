e16f7d4903e5486f22ef6b7860eef718d266358e
Laaguidi <aitlaaguidiyassine@gmail.com>
2024-01-15 06:59:57 +0100

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredTaskText, setEnteredTaskText] = useState('');
+
+  function taskInputHandler(enteredText) {
+    setEnteredTaskText(enteredText);
+  }
+
+  function addTaskHandler() {
+    console.log(enteredTaskText);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+              style={styles.textInput}
+              placeholder="Your course task!"
+              onChangeText={taskInputHandler}
+          />
+          <Button title="Add Task" onPress={addTaskHandler} />
+        </View>
+        <View style={styles.tasksContainer}>
+          <Text>List of goals...</Text>
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  tasksContainer: {
+    flex: 5,
   },
 });
